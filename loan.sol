43f69a8f8dc55f77ebfaaad8d0e17bc07b816e7f
jacob <jacob_a_reed@yahoo.com>
2020-04-09 19:15:21 +0000

    added a loan smart contract

---
+++ loan.sol
@@ -0,0 +1,20 @@
+//loan.sol
+/*
+<h2>Smart Contract</h2>
+<ul>
+<li>collects payments on the loan</li>
+<li>transfers ownership of business when three payments are missed in a row</li>
+<li>stores the payments into investor accounts</li>
+<li>gives loan to investor depending on how far along the business loan is</li>
+<li>transfers owners fee to owner</li>
+*/
+
+//collect payment
+
+//default on loan
+
+//add investor
+
+//constructor
+
+//investor loan
